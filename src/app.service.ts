a4e786894d1cf954ee1514dd593625388319c59f
EugeniaGramajo <gramajoeugenia@gmail.com>
2024-08-24 19:57:23 -0300

    keep alive

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,31 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor() {
+    this.startPing(); // Inicia el ping cuando se instancia el servicio
+  }
+
   getHello(): string {
+    console.log('sdadsds');
     return 'Hello World!';
+  }
+
+  keepAlive(): string {
+    return 'Server is alive!';
+  }
+
+  private startPing() {
+    setInterval(
+      () => {
+        fetch('https://jap-backend.onrender.com/alive')
+          .then(() =>
+            console.log('Ping realizado para mantener el servidor activo'),
+          )
+          .catch((error) =>
+            console.error('Error al realizar el ping:', error.message),
+          );
+      },
+      1 * 30 * 1000,
+    ); // Cada 5 minutos
   }
 }
